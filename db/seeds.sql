cdf15a45d7488ef0d0bf7e6995ce94c0105b02be
Angela Norris <cocoanorris@yahoo.com>
2018-08-12 16:39:52 -0400

    created file structure and db files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Avocado Cheddar Melt Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hot Rod Burger (topped with hot mustard and jalepenos');
+INSERT INTO burgers (burger_name) VALUES ('Piminto Pony Burger (topped with homemade piminto cheese');
