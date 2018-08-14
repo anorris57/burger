24d55039a180e1a8dd4995d6865542c0dc4eadfa
Angela Norris <cocoanorris@yahoo.com>
2018-08-14 17:22:53 -0400

    fixed spelling error

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "R00tang#57",
+  //Your password
+  password: "",
   database: "burgers_db"
 });
 
