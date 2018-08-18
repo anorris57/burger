117fd385422ac0d2e1a7ebf3f8e5e6c0b34dccc1
Angela Norris <cocoanorris@yahoo.com>
2018-08-18 15:02:52 -0400

    adding schema and seed files

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO burgers (burger_name)
+VALUES ("Avocado Swiss Melt Burger");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Tuna Dill Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Spam Cheddar Melt Burger");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pink Lady Apple Burger");
+
